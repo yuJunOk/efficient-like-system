59a97fdbaac812c5ed548aae71c3d291d1eb66f3
pengYuJun <1375841038@qq.com>
2025-06-28 16:14:22 +0800

    update: db_like.sql

--- sql/db_like.sql
+++ sql/db_like.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028 (8.0.28)
  File Encoding         : 65001
 
- Date: 28/06/2025 15:31:36
+ Date: 28/06/2025 16:13:22
 */
 
 SET NAMES utf8mb4;
@@ -32,11 +32,13 @@ CREATE TABLE `tb_blog`  (
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_blog
 -- ----------------------------
+INSERT INTO `tb_blog` VALUES (1, 1, '如何提高开发效率', 'img1.jpg', '要提高开发效率，可以采取以下方法：使用自动化工具减少重复劳动，复用代码和开源库避免重复造轮子，保持专注减少干扰，采用模块化开发提升协作效率，并持续学习新技术和快捷键来优化工作流程。', 0, '2025-06-28 15:45:00', '2025-06-28 16:12:10');
+INSERT INTO `tb_blog` VALUES (2, 1, 'Java的未来发展', 'img2.jpg', 'Java未来仍将保持主流地位，得益于其稳定性、跨平台特性和庞大的生态圈。随着云原生、微服务和GraalVM等技术的发展，Java将持续优化性能，适应现代化开发需求，在企业级应用和大数据领域保持竞争力。', 0, '2025-06-28 15:45:53', '2025-06-28 15:45:53');
 
 -- ----------------------------
 -- Table structure for tb_thumb
@@ -49,7 +51,7 @@ CREATE TABLE `tb_thumb`  (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_user_blog_id`(`user_id` ASC, `blog_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_thumb
